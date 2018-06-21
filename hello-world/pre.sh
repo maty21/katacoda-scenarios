be3913d501ab3fbab5dcf0cfeebcb5be1d5eb7c4
maty zisserman <maty21@gmail.com>
2018-06-21 14:59:50 +0300

    add pre

--- hello-world/pre.sh
+++ hello-world/pre.sh
@@ -1,8 +1,8 @@
-echo installing node.js 
+~~~~installing node.js~~~~ 
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs
 curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh
-echo installing helm 
+~~~~installing helm ~~~~ 
 chmod 700 get_helm.sh
 ./get_helm.sh
 echo cloning charts
