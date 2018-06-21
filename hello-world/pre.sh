9383210a1780aa704e819d52857517bf294a7476
maty zisserman <maty21@gmail.com>
2018-06-21 15:11:44 +0300

    add pre

--- hello-world/pre.sh
+++ hello-world/pre.sh
@@ -2,10 +2,10 @@
 echo '~~~installing node.js~~~~'
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
 sudo apt-get install -y nodejs
-echo '~~~~installing helm ~~~~'
-curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh
-chmod 700 get_helm.sh
-./get_helm.sh
+#echo '~~~~installing helm ~~~~'
+#curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get > get_helm.sh
+#chmod 700 get_helm.sh
+#./get_helm.sh
 echo '~~~cloning charts~~~'
 git clone https://github.com/maty21/katacoda-helm
 export VARNAME="my value"  
